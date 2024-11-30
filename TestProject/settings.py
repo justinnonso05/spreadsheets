ad4218100e52ce4a2bbf762a0ac25dd770268c09
justinnonso05 <chinonsoali2005@gmail.com>
2024-11-30 15:11:45 +0100

    election

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'justondev05@gmail.com'
